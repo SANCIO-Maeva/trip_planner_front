c9a78f3ed7fdade0851554673dcc71c6e0c0c84f
Maeva SANCIO <maevasancio@lesperserverants.fr>
2024-04-17 21:37:11 +0200

    feat(components):import leaflet

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Leaflet from "@/components/Leaflet.vue"
+import Itinerary from "@/components/Itinerary.vue"
 </script>
 
 <template>
     <Leaflet></Leaflet>
+    <Itinerary></Itinerary>
 </template>
