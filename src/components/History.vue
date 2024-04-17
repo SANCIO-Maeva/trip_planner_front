ca5d9f9b6f45a47860dfba33654c08e16f78e6b6
Maeva SANCIO <maevasancio@lesperserverants.fr>
2024-04-17 14:18:03 +0200

    feat(history): add data

---
+++ src/components/History.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+const trips = ref([]);
+
+const fetchTrips = async () => {
+    const response = await fetch("http://localhost:3000/v1/trips");
+    const data = await response.json();
+    trips.value = data;
+    console.log(data);
+};
+
+onMounted(() => {
+    fetchTrips();
+});
+</script>
+
+<template>
+    <h2 class="h6 mt-5">Derniers itinéraire</h2>
+    <div class="row justify-content-center" v-for="item in trips" :key="item.id">
+        <div class="card col-10 mt-2">
+            <div class="card-body">
+                {{ item.prompt }}
+            <i class="bi bi-chevron-right"></i>
+            </div>
+        </div>
+    </div>
+</template>
