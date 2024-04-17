694a099a451bb4722fd07282a184912a01a9bb7c
Maeva SANCIO <maevasancio@lesperserverants.fr>
2024-04-17 21:35:26 +0200

    feat(map)

--- src/components/Leaflet.vue
+++ src/components/Leaflet.vue
@@ -1,6 +1,26 @@
-<template>
+<script setup>
+import { ref, onMounted } from 'vue';
+import L from "leaflet";
 
-</template>
+onMounted(() => {
+  const map = L.map('map', {
+  center: L.latLng(55.665957, 12.550343),
+  zoom: 10,
+});
 
-<script>
+  const key = 'B3SpToQTffPiOQxtsGkV';
+
+  L.tileLayer(`https://api.maptiler.com/maps/topo-v2/{z}/{x}/{y}.png?key=${key}`,{
+    tileSize: 512,
+    zoomOffset: -1,
+    minZoom: 1,
+    attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
+    crossOrigin: true
+  }).addTo(map);
+  L.marker([55.665957, 12.550343]).addTo(map);
+});
 </script>
+
+<template>
+    <div id="map" style="height: 125vw;"></div>
+</template>
