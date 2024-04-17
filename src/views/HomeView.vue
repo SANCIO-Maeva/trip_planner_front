f2dea43f96fdfe517d6f2c4203a27f2caaf47c66
Maeva SANCIO <maevasancio@lesperserverants.fr>
2024-04-17 11:27:31 +0200

    fix(rename): HomeView.vue -> PromptView.vue

--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import Prompt from "@/components/Prompt.vue"
-import Historique from "@/components/Historique.vue"
-</script>
-
-<template>
-    <Prompt></Prompt>
-    <Historique></Historique>
-</template>
---
+++ src/views/PromptView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Prompt from "@/components/Prompt.vue"
+import Itinerary from "@/components/Itinerary.vue"
+</script>
+
+<template>
+    <h1 class="text-center">Planificateur de voyage</h1>
+    <h2 class="h5 text-center">généré par IA</h2>
+    <Prompt></Prompt>
+    <Itinerary></Itinerary>
+</template>
