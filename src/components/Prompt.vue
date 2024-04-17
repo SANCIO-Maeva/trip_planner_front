e724cd54c6da46d6a95f9c164214753e4760c293
Maeva SANCIO <maevasancio@lesperserverants.fr>
2024-04-17 11:26:30 +0200

    feat(textarea)

--- src/components/Prompt.vue
+++ src/components/Prompt.vue
@@ -1,4 +1,14 @@
 <script></script>
 <template>
-    <h1>Prompt</h1>
+    <div class="row justify-content-center mt-5 pt-5">
+        <div class="col-8">
+            <div class="mb-3">
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+            </div>
+            <div class="mt-5">
+                <button type="button" class="btn btn-secondary btn-sm">C'est parti</button>
+                <button type="button" class="btn btn-outline-secondary btn-sm">Voir un exemple</button>
+            </div>
+        </div>
+    </div>
 </template>
