c14ac0a52991cf2eb4160c3dba1fdeea7a8d605b
Maeva SANCIO <maevasancio@lesperserverants.fr>
2024-04-17 14:25:49 +0200

    feat(prompt): add logo

--- src/components/Prompt.vue
+++ src/components/Prompt.vue
@@ -1,11 +1,14 @@
 <script></script>
 <template>
-    <div class="row justify-content-center mt-5 pt-5">
+    <div class="row justify-content-center text-center mt-4">
+        <img class="logo mb-4" src="../assets/Frame.svg" alt="compass">
+        <h1>Planificateur de voyage</h1>
+        <h2 class="h5 mb-5">généré par IA</h2>
         <div class="col-8">
             <div class="mb-3">
                 <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
             </div>
-            <div class="mt-5">
+            <div class="mt-5 text-center">
                 <button type="button" class="btn btn-secondary btn-sm">C'est parti</button>
                 <button type="button" class="btn btn-outline-secondary btn-sm">Voir un exemple</button>
             </div>
