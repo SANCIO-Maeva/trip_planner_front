8889b7ed3b29d7541e146b80af6cfa4c8aa710b4
Maeva SANCIO <maevasancio@lesperserverants.fr>
2024-04-18 17:01:41 +0200

    feat(redirection)

--- src/components/HistoryList.vue
+++ src/components/HistoryList.vue
@@ -1,14 +1,23 @@
 <script setup>
 import { onMounted, ref } from "vue";
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
 const trips = ref([]);
+let tripId = null;
+
 
 const fetchTrips = async () => {
     const response = await fetch("http://localhost:3000/v1/trips");
     const data = await response.json();
     trips.value = data;
+    tripId = data.id;
     // console.log(data);
 };
+
+const redirection = async (tripId) => {
+    router.push({ name: 'about', params: { id: tripId } });
+}
 
 onMounted(() => {
     fetchTrips();
@@ -19,7 +28,7 @@ onMounted(() => {
     <section>
         <h2 class="h6 mt-5">Derniers itinéraire</h2>
         <div class="row justify-content-center" v-for="trip in trips" :key="trip.id">
-            <div class="card col-10 mt-2">
+            <div class="card col-10 mt-2" @click="redirection(trip.id)">
                 <div class="card-body">
                     {{ trip.prompt }}
                 <i class="bi bi-chevron-right"></i>
