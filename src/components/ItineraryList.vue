db0416f67f59ee5c643ba7146e9b08bb2e51f313
Maeva SANCIO <maevasancio@lesperserverants.fr>
2024-04-18 16:08:36 +0200

    fix(trip): data

--- src/components/ItineraryList.vue
+++ src/components/ItineraryList.vue
@@ -1,38 +1,35 @@
 <script setup>
-import { onMounted, ref, computed } from "vue";
+import { onMounted, ref } from "vue";
 import { useRoute } from 'vue-router';
 
-const data = ref(null);
 const route = useRoute();
 const tripId = route.params.id;
+const trip = ref({});
 
 onMounted(async () => {
     try {
-        const response = await fetch('http://localhost:3000/v1/trips');
+        const response = await fetch(`http://localhost:3000/v1/trips/${tripId}`);
         if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-        data.value = await response.json();
+        trip.value = await response.json();
     } catch (error) {
         console.error(error);
     }
 });
-
-const trip = computed(() => (data.value ? data.value.find(item => item.id === tripId) : null));
-
 </script>
 
 <template>
     <section>
         <div class="row">
-            <h2 class="col-10 mt-4" v-if="trip">{{ trip.prompt }}</h2>
+            <h2 class="col-10 mt-4">{{ trip.prompt }}</h2>
             <i class="mt-4 col-2 bi bi-pencil-square h2 "></i>
         </div>
         <h3 class="h6 mt-2 mb-2">Itinéraire</h3>
         <div class="row justify-content-center">
             <ol class="list-group-numbered">
-                <li class="list-group-item d-flex justify-content-between align-items-start" v-for="trip in trips" :key="trip.id">
-                    <div class="ms-2 me-auto" v-for="item in filteredData.output" :key="item.name">
-                        <div class="fw-bold">{{ item.name }}</div>
-                        <p>{{ item.description }}</p>
+                <li class="list-group-item d-flex justify-content-between align-items-start" v-for="location in trip.output" :key="location.id">
+                    <div class="ms-2 me-auto">
+                        <div class="fw-bold">{{ location.name }}</div>
+                        <p>{{ location.description }}</p>
                     </div>
                 </li>
             </ol>
