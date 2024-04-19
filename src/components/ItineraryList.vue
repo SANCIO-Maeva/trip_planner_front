a6aa79fde4cb8bcc3fbd26f30a171ea51075ade8
Maeva SANCIO <maevasancio@lesperserverants.fr>
2024-04-19 02:08:46 +0200

    fix(script & template):clean

--- src/components/ItineraryList.vue
+++ src/components/ItineraryList.vue
@@ -1,35 +1,28 @@
 <script setup>
-import { onMounted, ref } from "vue";
-import { useRoute } from 'vue-router';
+import { defineProps } from 'vue';
 
-const route = useRoute();
-const tripId = route.params.id;
-const trip = ref({});
-
-onMounted(async () => {
-    try {
-        const response = await fetch(`http://localhost:3000/v1/trips/${tripId}`);
-        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-        trip.value = await response.json();
-    } catch (error) {
-        console.error(error);
-    }
+const props = defineProps({
+  trip: {
+    type: Object,
+    required: true
+  }
 });
 </script>
 
 <template>
     <section>
         <div class="row">
-            <h2 class="col-10 mt-4">{{ trip.prompt }}</h2>
+            <h2 class="col-10 mt-4">{{ props.trip.prompt }}</h2>
             <i class="mt-4 col-2 bi bi-pencil-square h2 "></i>
         </div>
         <h3 class="h6 mt-2 mb-2">Itinéraire</h3>
         <div class="row justify-content-center">
             <ol class="list-group-numbered">
-                <li class="list-group-item d-flex justify-content-between align-items-start" v-for="location in trip.output" :key="location.id">
+                <li class="list-group-item d-flex justify-content-between align-items-start" v-for="location in props.trip.output" :key="location.id">
                     <div class="ms-2 me-auto">
                         <div class="fw-bold">{{ location.name }}</div>
                         <p>{{ location.description }}</p>
+                        <p>{{ location.location }}</p>
                     </div>
                 </li>
             </ol>
