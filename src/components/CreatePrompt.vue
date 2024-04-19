679051aac71074337fc21cda819cd651c1126f83
Maeva SANCIO <maevasancio@lesperserverants.fr>
2024-04-19 02:07:48 +0200

    fix(prompt):renamed

---
+++ src/components/CreatePrompt.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from "vue";
+import tripInstance from "@/services/trip.js";
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const trips = ref({
+    prompt: "",
+    output: "",
+    createdAt: "",
+    updatedAt: "",
+});
+
+
+const createPrompt =async () => {
+    console.log(trips.value);
+
+  let data = await tripInstance.create(
+    trips.value.prompt,
+    trips.value.output,
+    trips.value.createdAt,
+    trips.value.updatedAt,
+  );
+  trips.value = data;
+  const tripId = data.id;
+  console.log(tripId)
+  router.push({ name: 'about', params: { id: tripId } });
+}
+
+</script>
+
+
+<template>
+    <div class="row justify-content-center text-center mt-4">
+        <div class="col-6">
+            <img class="logo mb-3" src="../assets/Frame.svg" alt="compass">
+            <h1>Planificateur de voyage</h1>
+            <h2 class="h6 mb-4">généré par IA</h2>
+        </div>
+        <div class="col-8">
+            <div class="mb-2">
+                <textarea rows="5" v-model="trips.prompt"></textarea>
+            </div>
+            <div class="mt-5 text-center">
+                <button type="button" class="btn btn-secondary btn-sm" @click="createPrompt">C'est parti</button>
+                <button type="button" class="btn btn-outline-secondary btn-sm">Voir un exemple</button>
+            </div>
+        </div>
+    </div>
+</template>
