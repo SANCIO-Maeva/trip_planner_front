ee114e974c2e2667db84f3e2d42925b103f3c78a
Maeva SANCIO <maevasancio@lesperserverants.fr>
2024-04-19 15:33:48 +0200

    sup console.log

--- src/components/HistoryList.vue
+++ src/components/HistoryList.vue
@@ -12,7 +12,6 @@ const fetchTrips = async () => {
     const data = await response.json();
     trips.value = data;
     tripId = data.id;
-    // console.log(data);
 };
 
 const redirection = async (tripId) => {
