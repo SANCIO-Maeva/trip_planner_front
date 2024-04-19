2dbfe9999b2f21625b34182ce7469906039b61fe
Maeva SANCIO <maevasancio@lesperserverants.fr>
2024-04-19 02:04:06 +0200

    fix(map):clean

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,23 +1,15 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import L from "leaflet";
+import { defineProps } from 'vue';
 
-onMounted(() => {
-  const map = L.map('map', {
-  center: L.latLng(55.665957, 12.550343),
-  zoom: 10,
-});
-
-  const key = 'B3SpToQTffPiOQxtsGkV';
-
-  L.tileLayer(`https://api.maptiler.com/maps/topo-v2/{z}/{x}/{y}.png?key=${key}`,{
-    tileSize: 512,
-    zoomOffset: -1,
-    minZoom: 1,
-    attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-    crossOrigin: true
-  }).addTo(map);
-  L.marker([55.665957, 12.550343]).addTo(map);
+const props = defineProps({
+  trip: {
+    type: Object,
+    required: true,
+    output: {
+      type: Array,
+      required: true,
+    }
+  }
 });
 </script>
 
